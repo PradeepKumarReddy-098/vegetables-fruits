2a5211132c818ad8db5f289bc77d211472165003
PradeepKumarReddy-098 <pradeepmallela098@gmail.com>
2025-04-20 18:59:30 +0530

    first commit

--- my-app/src/components/PlaceOrders/index.jsx
+++ my-app/src/components/PlaceOrders/index.jsx
@@ -130,7 +130,7 @@ const PlaceOrders = () => {
                 <h1>No Item avaliable to place order.</h1>
                 <p>Go to home page to add itmes <Link to='/'>here</Link>.</p>
             </div>
-            </> 
+            </>
         )
     }
 
